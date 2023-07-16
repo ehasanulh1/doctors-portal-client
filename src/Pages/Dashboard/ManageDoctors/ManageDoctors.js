7ea0d1e2022b51d59602759537385896f3135ba4
ehasanulh1 <ehasanulh1@gmail.com>
2023-07-17 02:10:47 +0600

    doctor delete code done

--- src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
@@ -1,9 +1,99 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useState } from 'react';
+import Loading from '../../Shared/Loading/Loading';
+import ConfirmationModal from '../../Shared/ConfirmationModal/ConfirmationModal';
+import { toast } from 'react-hot-toast';
 
 const ManageDoctors = () => {
+    const [deleteDoctor, setDeleteDoctor] = useState(null);
+
+    const closeModal = () => {
+        setDeleteDoctor(null)
+    }
+
+    const { data: doctors, isLoading, refetch } = useQuery({
+        queryKey: ['doctors'],
+        queryFn: async () => {
+            try {
+                const res = await fetch('http://localhost:5000/doctors', {
+                    headers: {
+                        authorization: `bearer ${localStorage.getItem('accessToken')}`
+                    }
+                })
+                const data = await res.json();
+                return data;
+            }
+            catch (error) {
+
+            }
+        }
+    });
+
+    const handleDeleteDoctor = doctor => {
+        fetch(`http://localhost:5000/doctors/${doctor._id}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    toast.success(`Doctor ${doctor.name} deleted successfully!`)
+                    refetch();
+                }
+            })
+    }
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
     return (
         <div>
-            <h3 className='text-3xl mb-5'>Manage Doctors</h3>
+            <h3 className='text-3xl mb-5'>Manage Doctors: {doctors?.length}</h3>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Avatar</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Specialty</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            doctors?.map((doctor, i) => <tr key={doctor._id}>
+                                <th>{i + 1}</th>
+                                <td><div className="avatar">
+                                    <div className="mask rounded-full w-12 h-12">
+                                        <img src={doctor.image} alt="Avatar Tailwind CSS Component" />
+                                    </div>
+                                </div></td>
+                                <td>{doctor.name}</td>
+                                <td>{doctor.email}</td>
+                                <td>{doctor.specialty}</td>
+                                <td>
+                                    <label onClick={() => setDeleteDoctor(doctor)} htmlFor="my_modal_6" className="btn btn-sm btn-error">open modal</label>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+            {
+                deleteDoctor && <ConfirmationModal
+                    title={`Are you sure want to delete ${deleteDoctor.name}`}
+                    message={`If you delete ${deleteDoctor.name}. It cannot be undone.`}
+                    successButtonName='Delete'
+                    closeModal={closeModal}
+                    successAction={handleDeleteDoctor}
+                    modalData={deleteDoctor}
+                ></ConfirmationModal>
+            }
         </div>
     );
 };
