be99b6cac723c4718956d092400ad00297bdc464
ehasanulh1 <ehasanulh1@gmail.com>
2023-07-29 01:43:59 +0600

    payment method confirm

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,14 +1,28 @@
+import { Elements } from "@stripe/react-stripe-js";
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import CheckoutForm from "./CheckoutForm";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
+console.log(stripePromise);
 
 const Payment = () => {
   const booking = useLoaderData();
-  const { appointmentDate, price, treatment, slot} = booking;
+  const { appointmentDate, price, treatment, slot } = booking;
   console.log(booking);
   return (
     <div>
       <p className="text-3xl">Payment for {treatment}</p>
-      <p className="">Please pay <strong>${price}</strong> for your appointment on {appointmentDate} at {slot}</p>
+      <p className="">
+        Please pay <strong>${price}</strong> for your appointment on{" "}
+        {appointmentDate} at {slot}
+      </p>
+      <div className="w-96 my-5">
+        <Elements stripe={stripePromise}>
+          <CheckoutForm booking={booking} />
+        </Elements>
+      </div>
     </div>
   );
 };
---
+++ src/Pages/Dashboard/Payment/Payment_integrete_step.js
@@ -0,0 +1,8 @@
+/**
+ * stripe install
+ * card loadStripe
+ * card element
+ * card form
+ * stripe, element
+ * check card error and display error
+ */
